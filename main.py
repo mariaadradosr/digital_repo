e78342dfea452df36305cf2354010a5ce8617989
mariaadradosr <mariaadradosr@gmail.com>
2020-09-11 12:08:19 +0200

    creacion constantes y camnbio en main para comprobación de base

--- main.py
+++ main.py
@@ -3,24 +3,31 @@ import os
 from pathlib import Path
 import re
 import functions
+from constantes import base_inicial
 
 input_path = './input/'
 output_path = './output/'
 
 def main():
-    base = pd.read_csv('./output/base.csv',sep=';',decimal=',',encoding='CP1252')
-    saved_files = [e.name for e in sorted(Path(input_path).iterdir(), key=os.path.getmtime,reverse=True)]
+    # Comprobar si el archivo base está creado
+    if 'base.csv' not in os.listdir(output_path):
+        print('Creating base')
+        base = pd.DataFrame(data=base_inicial)
+    else:
+        base = pd.read_csv('./output/base.csv',sep=';',decimal=',',encoding='CP1252')
+    # saved_files = [e.name for e in sorted(Path(input_path).iterdir(), key=os.path.getmtime,reverse=True)]
+    saved_files = [e for e in os.listdir(input_path)]
     print('\nSAVED_FILES....',saved_files)
     base_files = list(base.file.unique())
     print('\nBASE_FILES....',base_files,'\n')
     to_add = [f for f in saved_files if f not in base_files]
     print('\nFiles to add .....',to_add,'\n')
-    #Comprobar si los archivos de base están en los guardados
+    # Comprobar si los archivos de base están en los guardados
     for file in base_files:
         if file not in saved_files:
             print('removing....',file)
             base = base.drop(base[base['file'] == file].index).reset_index()
-    #Añadir archivos a base
+    # Añadir archivos a base
     count = 0
     for file in saved_files:
         if file not in base_files:
@@ -42,7 +49,6 @@ def main():
         semanal.reset_index(inplace=True)
         semanal.to_csv('./output/semanal.csv',sep=';',index=False,decimal=',', encoding='CP1252')
         print('\nSEMANAL ...... UPDATED\n')
-# encoding='CP1252'
     base.to_csv('./output/base.csv',sep=';',index=False,decimal=',', encoding='CP1252')
     print('\nBASE ...... UPDATED\n')
 
